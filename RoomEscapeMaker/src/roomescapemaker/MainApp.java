30fb38f733231529680218dd0d0230edae88f8b1
xmcodings <xmuniv@cau.ac.kr>
2020-05-24 12:51:54 +0900

    connect Overview with Controller class

--- RoomEscapeMaker/src/roomescapemaker/MainApp.java
+++ RoomEscapeMaker/src/roomescapemaker/MainApp.java
@@ -1,23 +1,52 @@
 package roomescapemaker;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
+    
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			
+			Parent root = FXMLLoader.load(getClass().getResource("view/Overview.fxml"));
+
+	        primaryStage.setTitle("RoomExcapeMaker");
+	        primaryStage.setFullScreen(false);
+	        //Group root = new Group();
+
+	        Scene scene = new Scene(root, Color.BEIGE);
+	        primaryStage.setScene(scene);
+
+
+	        //primaryStage.setVisible(true);
+	        primaryStage.show();
+			
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
+	public void initRootLayout() {
+		
+		try {
+            
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("fail!!!");
+			System.out.println(e);
+		} 
+		
+		
+	}
+	
 	
 	public static void main(String[] args) {
 		launch(args);
---
+++ RoomEscapeMaker/src/roomescapemaker/view/Controller.java
@@ -0,0 +1,18 @@
+package roomescapemaker.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+
+public class Controller {
+
+    @FXML
+    private MenuItem file_close;
+
+    @FXML
+    void close_plogram(ActionEvent event) {
+
+    	
+    }
+
+}
