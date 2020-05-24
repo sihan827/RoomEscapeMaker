e6bda60d1f755638b2d35a09137b013aacffdc98
xmconding5 <mjkgrc@gmail.com>
2020-05-24 23:52:30 +0900

    canvas resize progress

--- RoomEscapeMaker/src/roomescapemaker/view/Controller.java
+++ RoomEscapeMaker/src/roomescapemaker/view/Controller.java
@@ -2,15 +2,25 @@ package roomescapemaker.view;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Controller {
+    
 
     @FXML
+    private Pane pane;
+	
+	@FXML
+    private Canvas mainCanvas;
+    
+	@FXML
     private Button playBtn;
 
     @FXML
@@ -33,9 +43,15 @@ public class Controller {
     	
     }
 
-    @FXML
+    @FXML 
     void playScene(ActionEvent event) {
-
+    	
+    	
     }
+    
+    void redraw() {
+    	
+    }
+    
 
 }
